5e82c3cb11fba85434855e9937916a6eda93deca
Tanner Nelson <tanner@bluebite.com>
2016-02-21 17:26:31 -0500

    naming

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "VaporStencil",
     dependencies: [
         .Package(url: "https://github.com/kylef/Stencil.git", majorVersion: 0),
-        .Package(url: "https://github.com/tannernelson/vapor.git", majorVersion: 0)
+        .Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0)
     ]
 )
