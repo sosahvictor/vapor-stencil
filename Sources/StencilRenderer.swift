6667b48190b0c0d26a16ee55446beb94bf483580
shnhrrsn <shnhrrsn@gmail.com>
2016-03-05 22:13:33 -0500

    Moved TemplateLoader to an instance var

--- Sources/StencilRenderer.swift
+++ Sources/StencilRenderer.swift
@@ -4,14 +4,16 @@ import Vapor
 
 public class StencilRenderer: RenderDriver {
 	public let namespace: Namespace
+	public let templateLoader: TemplateLoader
 
-	public init(namespace: Namespace = Namespace()) {
+	public init(namespace: Namespace = Namespace(), templateLoader: TemplateLoader? = nil) {
 		self.namespace = namespace ?? Namespace()
+		self.templateLoader = templateLoader ?? TemplateLoader(paths: [Path(View.resourceDir)])
 	}
 
 	public func render(template template: String, context: [String: Any]) throws -> String {
 		let c = Context(dictionary: context)
-		c["loader"] = TemplateLoader(paths: [Path(View.resourceDir)])
+		c["loader"] = self.templateLoader
 
 		let template = Template(templateString: template)
 		return try template.render(c, namespace: self.namespace)
