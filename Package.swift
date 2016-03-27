70c731343823c76bb75704c207ca33511c1200a3
Victor Sosa <sosah.victor@gmail.com>
2016-03-27 11:35:15 -0600

    Updating Stencil git repo to my fork

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "VaporStencil",
     dependencies: [
-        .Package(url: "https://github.com/kylef/Stencil.git", majorVersion: 0),
+        .Package(url: "https://github.com/sosahvictor/Stencil.git", majorVersion: 0),
         .Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0)
     ]
 )
